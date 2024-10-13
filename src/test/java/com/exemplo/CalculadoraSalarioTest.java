9f876ed4895d24281be05f43708e86cb58e888f4
taniacruzz <tanicruz112@gmail.com>
2024-10-13 18:11:51 -0300

    added tests to testador and gerente

--- src/test/java/com/exemplo/CalculadoraSalarioTest.java
+++ src/test/java/com/exemplo/CalculadoraSalarioTest.java
@@ -36,6 +36,30 @@ public class CalculadoraSalarioTest {
         CalculadoraSalario calculadora = new CalculadoraSalario();
         assertEquals(1275.0, calculadora.calcularSalarioLiquido(dba), 0.001);
     }
+    
+    // 5. Teste para TESTADOR com salário abaixo de 2000
+    @Test
+    public void testTestadorSalarioAbaixo2000() {
+        Funcionario testador = new Funcionario("Fernanda", "fernanda@exemplo.com", 1800.0, Cargo.TESTADOR);
+        CalculadoraSalario calculadora = new CalculadoraSalario();
+        assertEquals(1530.0, calculadora.calcularSalarioLiquido(testador), 0.001);
+    }
 
-    // Mais testes para GERENTE, TESTADOR, e outros cenários...
+    // 6. Teste para GERENTE com salário acima de 5000
+    @Test
+    public void testGerenteSalarioAcima5000() {
+        Funcionario gerente = new Funcionario("Roberto", "roberto@exemplo.com", 6000.0, Cargo.GERENTE);
+        CalculadoraSalario calculadora = new CalculadoraSalario();
+        assertEquals(4200.0, calculadora.calcularSalarioLiquido(gerente), 0.001);
+    }
+
+    // 7. Teste para GERENTE com salário abaixo de 5000
+    @Test
+    public void testGerenteSalarioAbaixo5000() {
+        Funcionario gerente = new Funcionario("Luciana", "luciana@exemplo.com", 4500.0, Cargo.GERENTE);
+        CalculadoraSalario calculadora = new CalculadoraSalario();
+        assertEquals(3600.0, calculadora.calcularSalarioLiquido(gerente), 0.001);
+    }
+
+ 
 }
